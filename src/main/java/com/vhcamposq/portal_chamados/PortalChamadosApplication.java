ad4ab6540ce6b7ca29dde44b093653b2ec8100c8
vhcamposq <vhcamposquadros@hotmail.com>
2026-03-13 15:59:54 -0300

    Subindo base do projeto

---
+++ src/main/java/com/vhcamposq/portal_chamados/PortalChamadosApplication.java
@@ -0,0 +1,13 @@
+package com.vhcamposq.portal_chamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalChamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vhcamposq/portal_chamados/PortalChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhcamposq.portal_chamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
